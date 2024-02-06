b3010bb4f2b03612cd6e062ea4a0119cecc19118
June <june.huang@tpisoftware.com>
2024-02-06 08:47:10 +0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by 高杉君 on 2024/2/6.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
